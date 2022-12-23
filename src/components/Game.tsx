a3bde48499f909a60b0d372c8387ce9d1a9c89ad
Ruslan Sharifullin <newwavecfy@gmail.com>
2022-12-24 01:44:06 +0300

    added dynamic data for Game

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -2,21 +2,23 @@ import React from 'react'
 import { useActions } from '../hooks/actions'
 import { useAppSelector } from '../hooks/redux'
 import '../index.scss'
+import { questions } from './data'
 
 function Game() {
   const { setStep } = useActions()
   const { step } = useAppSelector((state) => state.quiz)
+  const question = questions[step]
 
   return (
     <>
       <div className="progress">
         <div style={{ width: '50%' }} className="progress__inner"></div>
       </div>
-      <h1>Что такое useState?</h1>
+      <h1>{question.title}</h1>
       <ul>
-        <li>Это функция для хранения данных компонента</li>
-        <li>Это глобальный стейт</li>
-        <li>Это когда на ты никому не нужен</li>
+        {question.variants.map((text) => (
+          <li key={text}>{text}</li>
+        ))}
       </ul>
     </>
   )
